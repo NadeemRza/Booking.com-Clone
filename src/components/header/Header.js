42c979fdc91dac0f2a2c06b406383cd16bbe80c7
Nadeem <nr859696@gmail.com>
2022-11-29 20:34:45 +0530

    added hotel list and hotel details page

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -15,8 +15,12 @@ import { DateRange } from "react-date-range";
 import "react-date-range/dist/styles.css"; // main css file
 import "react-date-range/dist/theme/default.css"; // theme css file
 import { format } from "date-fns";
+import { useNavigate } from "react-router-dom";
 
 const Header = ({ type }) => {
+
+  const [destination, setDestination] = useState('');
+
   const [openDate, setOpenDate] = useState(false);
 
   const [date, setDate] = useState([
@@ -35,6 +39,8 @@ const Header = ({ type }) => {
     room: 1,
   });
 
+  const navigate = useNavigate();
+
   const handleOption = (name, operation) => {
     setOptions((prev) => {
       return {
@@ -42,6 +48,10 @@ const Header = ({ type }) => {
         [name]: operation === "i" ? options[name] + 1 : options[name] - 1,
       };
     });
+  };
+
+  const handleSearch = () => {
+    navigate('/hotels', {state: { destination, date, options }});
   };
 
   return (
@@ -84,6 +94,7 @@ const Header = ({ type }) => {
               type="text"
               placeholder="Enter Place To Explore"
               className="headerSearchInput"
+              onChange={e=>setDestination(e.target.value)}
             />
           </div>
           <div className="headerSearchItem">
@@ -102,6 +113,7 @@ const Header = ({ type }) => {
                 moveRangeOnFirstSelection={false}
                 ranges={date}
                 className="date"
+                minDate={new Date()}
               />
             )}
           </div>
@@ -176,7 +188,7 @@ const Header = ({ type }) => {
             )}
           </div>
           <div className="headerSearchItem">
-            <button className="headerBtn">Search</button>
+            <button className="headerBtn" onClick={handleSearch}>Search</button>
           </div>
         </div>
         </>}
