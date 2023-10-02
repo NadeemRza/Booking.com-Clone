956325846042ae586a6ec519407c20a7c971d346
NadeemRza <nr859696@gmail.com>
2023-10-02 19:19:08 +0530

    added middlewares

---
+++ backend/api/routes/users.js
@@ -0,0 +1,27 @@
+import express from "express";
+import { deleteUser, getAllUser, getUser, updateUser } from "../controllers/user.js";
+import { verifyToken, verifyUser } from "../utils/verifyToken.js";
+
+const router = express.Router();
+
+router.get('/checkauthentication', verifyToken, (req,res,next)=>{
+    res.send("Hello user, you are logged in");
+})
+
+router.get('/checkuser/:id', verifyUser, (req,res,next)=>{
+    res.send("Hello user, you are logged in and you can delete your account.");
+})
+
+//UPDATE
+router.put("/:id", updateUser);
+
+//DELETE
+router.delete("/:id", deleteUser);
+
+//GET
+router.get("/:id", getUser);
+
+//GET ALL
+router.get("/", getAllUser);
+
+export default router;
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,61 @@
 import express from "express";
 const app = express()
-const port = 5000
+const port = 5000;
+
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config();
+
+import cookieParser from "cookie-parser";
+
+import authRoute from "./api/routes/auth.js";
+import usersRoute from "./api/routes/users.js";
+import hotelsRoute from "./api/routes/hotels.js";
+import roomsRoute from "./api/routes/rooms.js";
+
+const connectMongodb = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("connected to DB");
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+mongoose.connection.on("disconnected", ()=>{
+    console.log("DB Disconnected");
+})
+
+mongoose.connection.on("connected", ()=>{
+    console.log("DB Connected");
+})
+
+//middlewares
+app.use(cookieParser())
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/hotels", hotelsRoute);
+app.use("/api/rooms", roomsRoute);
+
+//error handling
+app.use((err, req,res,next)=>{
+    const errorStatus = err.status || 500;
+    const errorMessage = err.message || "Something went wrong.";
+    return res.status(errorStatus).json({
+        success: false,
+        status: errorStatus,
+        message: errorMessage,
+        stack: err.stack
+    })
+})
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    connectMongodb();
+    console.log(`Example app listening on port ${port}`)
 })
